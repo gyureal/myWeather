89ae93cc2837ceb4da123f3a51c9e57b23c0006a
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-01 00:04:19 +0900

    DiaryController, DiaryService

--- src/main/java/com/example/myweather/controller/DiaryController.java
+++ src/main/java/com/example/myweather/controller/DiaryController.java
@@ -1,4 +1,26 @@
 package com.example.myweather.controller;
 
+import com.example.myweather.service.DiaryService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
 public class DiaryController {
+    private final DiaryService diaryService;
+
+    public DiaryController(DiaryService diaryService) {
+        this.diaryService = diaryService;
+    }
+
+    @PostMapping("/create/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime date,
+                     @RequestBody String text) {
+        diaryService.createDiary(date, text);
+
+    }
 }
--- src/main/java/com/example/myweather/service/DiaryService.java
+++ src/main/java/com/example/myweather/service/DiaryService.java
@@ -1,4 +1,22 @@
 package com.example.myweather.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class DiaryService {
+
+    @Value("${openweathermap.key}")
+    String apiKey;
+
+    public void createDiary(LocalDateTime date, String text) {
+
+    }
+
+    private void getWeatherString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?p=seoul&appid=" + apiKey;
+        System.out.println(apiUrl);
+    }
 }
