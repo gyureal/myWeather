f8d5ed77728fa1fcf061e71e3c7a9c1cd2ffaee3
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-01 00:47:41 +0900

    json 파싱 로직 추가

--- src/main/java/com/example/myweather/service/DiaryService.java
+++ src/main/java/com/example/myweather/service/DiaryService.java
@@ -1,9 +1,20 @@
 package com.example.myweather.service;
 
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DiaryService {
@@ -11,12 +22,55 @@ public class DiaryService {
     @Value("${openweathermap.key}")
     String apiKey;
 
-    public void createDiary(LocalDateTime date, String text) {
+    public void createDiary(LocalDate date, String text) {
+        String weatherString = getWeatherString();
 
+        Map<String, Object> parseWeather = parseWeather(weatherString);
     }
 
-    private void getWeatherString() {
-        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?p=seoul&appid=" + apiKey;
-        System.out.println(apiUrl);
+    private String getWeatherString() {
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
+
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            int responseCode = conn.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            return response.toString();
+
+        } catch (Exception e) {
+            return "failed to response";
+        }
+    }
+
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        } catch (ParseException e){
+            throw new RuntimeException(e);
+        }
+
+        Map<String, Object> resultMap = new HashMap<>();
+        JSONObject main = (JSONObject) jsonObject.get("main");
+        resultMap.put("temp", main.get("temp"));
+        JSONObject weather = (JSONObject) jsonObject.get("weather");
+        resultMap.put("main", main.get("main"));
+        resultMap.put("icon", main.get("icon"));
+        return resultMap;
     }
 }
