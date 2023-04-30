6f4450fc06f94ca92d18266213175a5d3e257c72
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-05-01 00:48:01 +0900

    LocalDate

--- src/main/java/com/example/myweather/controller/DiaryController.java
+++ src/main/java/com/example/myweather/controller/DiaryController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RestController
@@ -18,7 +19,7 @@ public class DiaryController {
     }
 
     @PostMapping("/create/diary")
-    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime date,
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                      @RequestBody String text) {
         diaryService.createDiary(date, text);
 
